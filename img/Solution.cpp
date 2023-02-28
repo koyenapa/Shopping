59f9d58eeaf87b15d32e9e864c8b524268d8d964
koyenapa <121396381+koyenapa@users.noreply.github.com>
2023-02-28 22:18:32 +0530

    Add files via upload

---
+++ img/Solution.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/find-the-duplicate-number
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+     int temp=0;
+     int i=0;
+      while(i<nums.size())
+	{
+		if(nums[i]!=nums[nums[i]-1])
+		{
+		   swap(nums[i],nums[nums[i]-1]);
+	    }
+		else
+		{
+			i++;
+		}   
+    }for(int i=0;i<nums.size();i++)
+	{
+		if(nums[i]!=i+1)
+		{
+			return nums[i];
+        }   
+    }
+        return -1;
+    }    
+};
